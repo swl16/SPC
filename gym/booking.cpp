b1c296051811bd5d05df9ed06720796640a527ad
Potatwoo-o <potatwoo0214@gmail.com>
2026-08-09 17:02:45 +0800

    .

--- gym/booking.cpp
+++ gym/booking.cpp
@@ -11,7 +11,36 @@
 
 using namespace std;
 
+int generateBookingID(const string& filename) {
+    ifstream file(filename);
+    if (!file.is_open()) return 1001; // Default start ID
+
+    int lastID = 1000;
+    string line;
+    while (getline(file, line)) {
+        if (line.empty()) continue;
+        stringstream ss(line);
+        int currentID;
+        if (ss >> currentID) {
+            lastID = currentID;
+        }
+    }
+    file.close();
+    return lastID + 1;
+}
+
 void bookClass(Member member) {
 	ifstream classFile("classes.txt");
-	jj
+    if (!classFile.is_open()) {
+        cerr << "Error opening classes.txt!" << endl;
+        return;
+    }
+
+    vector<Class> availableClasses;
+    Class c;
+
+    while(classFile >> c.classID)
+    cout << "================================================" << endl;
+    cout << "                  CLASS BOOKING                 " << endl;
+    cout << "================================================" << endl << endl;
 }
